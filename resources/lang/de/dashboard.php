e2a79e9dc2a9e6aa30dbac5ceb4e0b38df66eb4e
Marco Curatitoli <makroxyz@gmail.com>
2026-06-15 10:09:07 +0200

    Add German language file for dashboard translations

---
+++ resources/lang/de/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Dashboard',
+    'customize_button' => 'Mein Dashboard anpassen',
+    'save_button' => 'Speichern',
+    'cancel_button' => 'Abbrechen',
+    'available_widgets' => 'Verfügbare Widgets',
+    'saved_successfully' => 'Gespeichert',
+];
