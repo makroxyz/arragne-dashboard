27d4180bb23d18ac93406985543ff38675eab15f
Marco Curatitoli <makroxyz@gmail.com>
2026-06-15 10:08:12 +0200

    Add English language file for dashboard translations

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Dashboard',
+    'customize_button' => 'Customize My Dashboard',
+    'save_button' => 'Save',
+    'cancel_button' => 'Cancel',
+    'available_widgets' => 'Available Widgets',
+    'saved_successfully' => 'Saved',
+];
