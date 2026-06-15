ab0aba4f6b811f5a3239b8299148c91f4933da4f
Marco Curatitoli <makroxyz@gmail.com>
2026-06-15 09:56:16 +0200

    Add Spanish language file for dashboard translations

---
+++ resources/lang/es/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Panel de control',
+    'customize_button' => 'Personalizar mi panel de control',
+    'save_button' => 'Guardar',
+    'cancel_button' => 'Cancelar',
+    'available_widgets' => 'Widgets Disponibles',
+    'saved_successfully' => 'Guardado',
+];
