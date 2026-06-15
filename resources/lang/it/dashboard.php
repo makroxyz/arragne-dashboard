a523c8a2446a4e6d9a7078137149d7791ab21f23
Marco Curatitoli <makroxyz@gmail.com>
2026-06-15 10:08:50 +0200

    Add Italian language file for dashboard translations

---
+++ resources/lang/it/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Dashboard',
+    'customize_button' => 'Personalizza la mia dashboard',
+    'save_button' => 'Salva',
+    'cancel_button' => 'Annulla',
+    'available_widgets' => 'Widget Disponibili',
+    'saved_successfully' => 'Salvato',
+];
